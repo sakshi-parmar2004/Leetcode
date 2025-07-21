055ad329384e5d9c4c217656c8c440843dc8027b
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-07-21 13:01:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (38.85%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1,j=0;
-        while(i>=0 && j<n)
-        {
-            if(nums1[i]>nums2[j])
-            {
-                swap(nums1[i], nums2[j]);
-                i--;
-                j++;
+        int len = m + n;
+        int gap = (len + 1) / 2;
+
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // Case 1: both in nums1
+                if (left < m && right < m) {
+                    if (nums1[left] > nums1[right]) {
+                        swap(nums1[left], nums1[right]);
+                    }
+                }
+                // Case 2: left in nums1, right in nums2
+                else if (left < m && right >= m) {
+                    if (nums1[left] > nums2[right - m]) {
+                        swap(nums1[left], nums2[right - m]);
+                    }
+                }
+                // Case 3: both in nums2
+                else if (left >= m && right >= m) {
+                    if (nums2[left - m] > nums2[right - m]) {
+                        swap(nums2[left - m], nums2[right - m]);
+                    }
+                }
+
+                left++;
+                right++;
             }
-            else
-            {
-                break;
-            }
-            
+
+            if (gap == 1) break;
+            gap = (gap + 1) / 2;
         }
-        sort(nums1.begin() , nums1.begin()+m);
-        sort(nums2.begin() , nums2.end());
-        j=0;
-        for(int i=m;i<m+n;i++)
-        {
-            nums1[i]= nums2[j++];
+
+        // Copy sorted nums2 to nums1's end
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
 };
