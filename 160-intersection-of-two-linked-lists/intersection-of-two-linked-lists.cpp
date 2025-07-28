d30828d303b4e3571bbe3a40fe3e323cf84eb560
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-07-28 19:07:31 +0530

    Time: 38 ms (49.31%) | Memory: 18.3 MB (98.23%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -6,43 +6,20 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
-    ListNode* solve(ListNode *temp1, ListNode *temp2,int count)
-    {
-        while(count>0)
-        {
-            temp2=temp2->next;
-            count--;
-
-        }
-
-        while(temp1!=NULL && temp2!=NULL && temp1!=temp2)
-        {
-            temp1=temp1->next;
-            temp2=temp2->next;
-        }
-        return temp1;
-    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1= headA , *temp2=headB;
-       int ct1=0,ct2=0;
-        while(temp1)
-        {
-           ct1++; 
-           temp1=temp1->next;
-        }
-        while(temp2)
-        {
-          ct2++; 
-           temp2=temp2->next;
-        }
-        if(ct1<ct2)
-        {
-           return solve(headA,headB,ct2-ct1);
-        }
-        return solve(headB,headA, ct1-ct2);
+        if (!headA || !headB) return NULL;
 
-        
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+
+        return a;  // Can be NULL if no intersection
     }
 };
