e7d9e79c760ca43a683cd852f5540ad6238f87b9
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-15 21:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (61.66%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n =ratings.size();
+        vector<int>ans(n,1);
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i-1]<ratings[i])
+            {
+              ans[i]+=ans[i-1];
+            }
+        }
+         for(int i=n-2;i>=0;i--)
+        {
+            if(  ratings[i]>ratings[i+1] && ans[i]<=ans[i+1] )
+            {
+
+              ans[i]=ans[i+1]+1;
+            }
+        }
+        int result=0;
+        for(int i:ans)
+        {
+            result+=i;
+        }
+        return result;
+
+        
+    }
+};
