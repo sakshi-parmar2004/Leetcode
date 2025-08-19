a0c9f50b34d4da7e83202e60582cb83be40a64d3
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-20 00:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 111.2 MB (74.50%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans=0,count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                count++;
+                ans+=count;
+            }
+            else{
+                count=0;
+            }
+        }
+        return ans;
+        
+    }
+};
