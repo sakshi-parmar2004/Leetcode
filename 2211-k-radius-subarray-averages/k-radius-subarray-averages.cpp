3273f230e5161df49f45263f36dce0a244afa408
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-26 21:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 141.5 MB (29.52%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int n =nums.size();
-         vector<int>ans(n,-1);
-         long long sum=0,i=0,j=0,wind=2*k+1;
-         while(j<n)
-         {
-            sum+=nums[j];
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        if (k == 0) return nums; // each element is its own average
+        if (2*k+1 > n) return ans; // window too large for any average
 
-            if(j-i+1== wind)
-            {
-                int val = sum/(2*k+1);
-                int ind= i+k;
-                ans[ind]=val;
-                sum-=nums[i];
-                i++;
-            }
-            j++;
-         }
-         return ans;
+        vector<long long> prefix(n, 0);
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
 
-        
+        for (int i = 0; i + 2*k < n; i++) {
+            long long total = prefix[i+2*k] - (i > 0 ? prefix[i-1] : 0);
+            ans[i+k] = total / (2*k+1);
+        }
+
+        return ans;
     }
 };
