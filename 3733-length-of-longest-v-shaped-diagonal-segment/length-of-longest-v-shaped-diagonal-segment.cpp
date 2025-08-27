ea047883035e19438f05b0ad39130521a59f9ad1
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-27 22:04:31 +0530

    Time: 1223 ms (37.43%) | Memory: 107.5 MB (64.71%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,50 +1,74 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<int>> grid;
+    int m,n;
+   vector<vector<int>>arr;
+   int solve(int i,int j,int dir,int value,bool turn)
+        {
+            if(i<0 || j<0 || i>=m || j>=n || arr[i][j]!=value)
+            {
+                return 0;
+            }
+           int ans=0;
+            //option1 direction is towards to bottom right
+            if(dir==0)
+            {
+                   ans= 1+ solve(i+1,j+1,dir,2-value,turn);
+                   if(!turn)
+                   {
+                    ans=max(ans, 1+solve(i+1,j-1,1,2-value,!turn));
+                   }
+            }
+            //option2 direction is towards to bottom left
+            if(dir==1)
+            {
+                   ans= 1+ solve(i+1,j-1,dir,2-value,turn);
+                   if(!turn)
+                   {
+                    ans=max(ans, 1+solve(i-1,j-1,2,2-value,!turn));
+                   }
+            }
+           //option3 direction is towards to top left
+            if(dir==2)
+            {
+                   ans= 1+ solve(i-1,j-1,dir,2-value,turn);
+                   if(!turn)
+                   {
+                    ans=max(ans, 1+solve(i-1,j+1,3,2-value,!turn));
+                   }
+            }
+            //option4 direction is towards to top right
+            if(dir==3)
+            {
+                   ans= 1+ solve(i-1,j+1,dir,2-value,turn);
+                   if(!turn)
+                   {
+                    ans=max(ans, 1+solve(i+1,j+1,0,2-value,!turn));
+                   }
+            }
 
-    // dir: 1=top-right, 2=bottom-right, 3=bottom-left, 4=top-left
-    int solve(int r, int c, bool turned, int dir, int expected) {
-        if (r < 0 || c < 0 || r >= m || c >= n) return 0;
-        if (grid[r][c] != expected) return 0;
+            return ans;
 
-        int ans = 0;
 
-        if (dir == 1) { // top-right
-            ans = 1 + solve(r - 1, c + 1, turned, 1, 2 - expected);
-            if (!turned) ans = max(ans, 1 + solve(r + 1, c + 1, true, 2, 2 - expected));
         }
-        else if (dir == 2) { // bottom-right
-            ans = 1 + solve(r + 1, c + 1, turned, 2, 2 - expected);
-            if (!turned) ans = max(ans, 1 + solve(r + 1, c - 1, true, 3, 2 - expected));
-        }
-        else if (dir == 3) { // bottom-left
-            ans = 1 + solve(r + 1, c - 1, turned, 3, 2 - expected);
-            if (!turned) ans = max(ans, 1 + solve(r - 1, c - 1, true, 4, 2 - expected));
-        }
-        else if (dir == 4) { // top-left
-            ans = 1 + solve(r - 1, c - 1, turned, 4, 2 - expected);
-            if (!turned) ans = max(ans, 1 + solve(r - 1, c + 1, true, 1, 2 - expected));
-        }
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        m=grid.size(),n=grid[0].size();
+        arr=grid;
+        int ans=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    ans=max(ans,1+ solve(i+1,j+1,0,2,false));
+                    ans=max(ans,1+ solve(i+1,j-1,1,2,false));
+                    ans=max(ans,1+ solve(i-1,j-1,2,2,false));
+                    ans=max(ans,1+ solve(i-1,j+1,3,2,false));
 
-        return ans;
-    }
-
-    int lenOfVDiagonal(vector<vector<int>>& g) {
-        grid = g;
-        m = grid.size(), n = grid[0].size();
-
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) { // must start with 1
-                    ans = max(ans, 1 + solve(i - 1, j + 1, false, 1, 2));
-                    ans = max(ans, 1 + solve(i + 1, j + 1, false, 2, 2));
-                    ans = max(ans, 1 + solve(i + 1, j - 1, false, 3, 2));
-                    ans = max(ans, 1 + solve(i - 1, j - 1, false, 4, 2));
                 }
             }
         }
         return ans;
+        
     }
 };
