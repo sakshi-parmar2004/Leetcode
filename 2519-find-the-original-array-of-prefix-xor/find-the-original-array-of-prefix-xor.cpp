1a10ae4b93fa3d48dcb3dfcdba81e9636ad047ac
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-28 11:05:28 +0530

    Time: 15 ms (8.58%) | Memory: 80.5 MB (74.15%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int solve(int a,int b)
-    {
-        int ans=a;
-        while(b>0)
-        {
-    
-            int bit_of_b = b&1;
-            //ooposite bits as b is 1 bits the ans and a will have opp bit in this case
-            if(bit_of_b!=0)
-            {
-                 ans=ans^1;
+    int solve(int a, int b) {
+        int ans = a;
+        int mask=1;
+        while (b > 0) {
+            int bit_of_b =  b & 1; // ooposite bits as b is 1 bits the ans and a will have opp
+            if (bit_of_b ) {
+                ans = ans ^ mask;
             }
-            
-            b>>1;
-            1<<1;
-
+            b >>= 1;
+            mask <<= 1;
         }
         return ans;
     }
     vector<int> findArray(vector<int>& pref) {
-        int n=pref.size();
-        vector<int>ans(n);
-        ans[0]=pref[0];
-        for(int i=1;i<pref.size();i++)
-        {
-            ans[i]= pref[i-1]^pref[i];
+        int n = pref.size();
+        vector<int> ans(n);
+        ans[0] = pref[0];
+        for (int i = 1; i < pref.size(); i++) {
+            ans[i] = solve(pref[i - 1], pref[i]);
         }
-        
         return ans;
     }
 };
