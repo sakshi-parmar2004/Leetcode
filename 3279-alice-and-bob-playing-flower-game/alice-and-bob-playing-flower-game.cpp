cb8ba7cf210761e87662cb6433a0e67c8114653d
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-29 08:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (12.44%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                int odd_number = 0;
-                // how many odd number in range 1-m
-                if (m % 2 == 0) {
-                    odd_number = m / 2;
-                } else {
-                    odd_number = (m / 2) + 1;
-                }
-                count += odd_number;
-            } else {
-                // how many even umbers in range 1-m
-
-                int even_number = m / 2;
-
-                count += even_number;
-            }
+        long long odd_number_n=n/2;
+        if(n%2!=0)
+        {
+            odd_number_n+=1;
+            
         }
-        return count;
+        long long even_number_n=n/2;
+
+        long long odd_number_m=m/2;
+        if(m%2!=0)
+        {
+            odd_number_m+=1;
+            
+        }
+        long long even_number_m=m/2;
+
+        long long answer = odd_number_n * even_number_m + even_number_n*odd_number_m;
+        return answer;
+
+
+        
     }
 };
