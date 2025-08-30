fba485f7733398b5f3ac0a2170df55fbbe1fc44d
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-30 11:50:09 +0530

    Time: 19 ms (6.32%) | Memory: 25.9 MB (7.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,57 +1,28 @@
 class Solution {
 public:
-    bool solve(int sr, int er, int sc, int ec, vector<vector<char>>& board) {
-        unordered_set<char> st;
-        for (int i = sr; i <= er; i++) {
-            for (int j = sc; j <= ec; j++) {
-                 if (board[i][j] == '.') {
-                    continue;
-                }
-                if (st.find(board[i][j]) != st.end()) {
-                    return false;
-                }
-                st.insert(board[i][j]);
-            }
-        }
-        return true;
-    }
     bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
         int row = board.size(), col = board[0].size();
-       
-        for (int i = 0; i < row; i++) {
-             unordered_set<char> st1;
-            for (int j = 0; j < col; j++) {
-                if (board[i][j] == '.') {
-                    continue;
-                }
-                if (st1.find(board[i][j]) != st1.end()) {
-                    return false;
-                }
-                st1.insert(board[i][j]);
-            }
-        }
-
         
-        for (int i = 0; i < col; i++) {
-            unordered_set<char> st2;
-            for (int j = 0; j < row; j++) {
-                if (board[j][i] == '.') {
-                    continue;
-                }
-                if (st2.find(board[j][i]) != st2.end()) {
-                    return false;
-                }
-                st2.insert(board[j][i]);
-            }
-        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == '.') continue; // skip empty cells
 
-        for (int i = 0; i < row; i += 3) {
-            int er = i + 2;
-            for (int j = 0; j < col; j += 3) {
-                int ec = j + 2;
-                if (!solve(i, er, j, ec, board)) {
+                char num = board[i][j]; // use the char directly
+
+                string row_num = string(1, num) + " in row " + to_string(i);
+                string col_num = string(1, num) + " in col " + to_string(j);
+                string box = string(1, num) + " in box " + to_string(i/3) + "-" + to_string(j/3);
+
+                if (st.find(row_num) != st.end() || 
+                    st.find(col_num) != st.end() || 
+                    st.find(box) != st.end()) {
                     return false;
                 }
+
+                st.insert(row_num);
+                st.insert(col_num);
+                st.insert(box);
             }
         }
         return true;
