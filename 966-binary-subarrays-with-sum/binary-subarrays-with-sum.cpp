9d03e9dd81c7c8c1d2a827ab66663879815424ad
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-08-30 18:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (80.38%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int count=0,sum=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
+    int solve(int i,vector<int>& nums, int goal)
+    {
+        if(goal<0)
         {
-            sum+=nums[i];
-            if(sum==goal)
-            {
-                count++;
-            }
-            int value = sum-goal;
-            if(mp.find(value)!=mp.end())
-            {
-                count+=mp[value];
-            }
-            mp[sum]++;
+            return 0;
         }
-        return count;
+        int ans=0 ,j=0,sum=0;
+        while(j<nums.size())
+        {
+            sum+=nums[j];
+            while(sum>goal)
+            {
+                sum-=nums[i];
+                i++;
+            }
+            ans += (j-i+1);
+            j++;
+        }
+        return ans;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
         
+        int value1= solve(0,nums,goal);
+        int value2 = solve(0,nums,goal-1);
+        return value1-value2;
     }
 };
