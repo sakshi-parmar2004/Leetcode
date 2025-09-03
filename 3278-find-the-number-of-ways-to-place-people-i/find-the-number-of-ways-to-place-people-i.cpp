b0dd8d11d6580090d8f92e3b0936359b4e1eadf2
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-09-03 23:17:43 +0530

    Time: 3 ms (76.09%) | Memory: 32.7 MB (21.74%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -4,30 +4,25 @@ public:
         int n = points.size();
         int count = 0;
 
+        // Sort by x descending, if tie by y descending
+        sort(points.begin(), points.end(), [](vector<int>& p1, vector<int>& p2) {
+            if (p1[0] == p2[0]) return p1[1] > p2[1];
+            return p1[0] < p2[0];
+        });
+
         for (int i = 0; i < n; i++) {
-            int x1 = points[i][0], y1 = points[i][1]; // A (upper-left)
+            int x1 = points[i][0], y1 = points[i][1]; // candidate A
+            int max_y = -1;
 
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
+            for (int j = i+1; j < n; j++) {
+                int x2 = points[j][0], y2 = points[j][1]; // candidate B
 
-                int x2 = points[j][0], y2 = points[j][1]; // B (lower-right)
+                if (y2 > y1) continue; // must be below or equal
 
-                // Condition: A is upper-left of B
-                if (!(x1 <= x2 && y1 >= y2)) continue;
-
-                bool flag = false;
-                for (int k = 0; k < n; k++) {
-                    if (i == k || j == k) continue;
-
-                    int x = points[k][0], y = points[k][1];
-                    // Check if point k is inside or on rectangle A-B
-                    if (x1 <= x && x <= x2 && y2 <= y && y <= y1) {
-                        flag = true;
-                        break;
-                    }
+                if (y2 > max_y) {
+                    max_y = y2;
+                    count++;
                 }
-
-                if (!flag) count++;
             }
         }
 
