ba5713a457a465ae3bcd2307255bd861aab9e80b
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-09-11 09:03:26 +0530

    Time: 8 ms (85.68%) | Memory: 18.5 MB (15.37%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        unordered_set<int>st={'a','e','i','o','u','A','E','I','O','U'};
-        string ans =s;
-        string str="";
-        for(char i:s)
-        {
-            if(st.find(i)!=st.end())
-            {
-                str+=i;
+        unordered_set<char> st = {'a','e','i','o','u','A','E','I','O','U'};
+        string ans = s;
+        vector<int> freq(128, 0); // ASCII size is 128
+
+        // count frequency of vowels
+        for (char c : s) {
+            if (st.find(c) != st.end()) {
+                freq[(int)c]++;
             }
         }
-        
-        sort(str.begin(),str.end());
-        int k=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(st.find(s[i])!=st.end())
-            {
-                ans[i]=str[k++];
+
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (st.find(s[i]) != st.end()) {
+                while (k < 128 && freq[k] == 0) k++;
+                ans[i] = (char)k;
+                freq[k]--;
             }
         }
+
         return ans;
-        
     }
 };
