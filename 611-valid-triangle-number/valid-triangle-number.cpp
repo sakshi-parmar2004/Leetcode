9fbe7cc9a73f9312c0c838fa69cb8bf21c49ad9e
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-09-26 10:41:05 +0530

    Time: 35 ms (42.26%) | Memory: 16.7 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    int bin_search(int l ,int r ,int target, vector<int>& nums )
-    {
-        int result= r+1;
-        while(l<=r)
-        {
-            int mid = l+(r-l)/2;
-            if(nums[mid]>=target)
-            {
+    int bin_search(int l, int r, int target, vector<int>& nums) {
+        int result = r + 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
                 result = mid;
-                r=mid-1;
-            }
-            else
-            {
+                r = mid - 1;
+            } else {
 
-                l=mid+1;
+                l = mid + 1;
             }
         }
         return result;
-    
-
-
     }
     int triangleNumber(vector<int>& nums) {
-        int n= nums.size(),ans=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==0)continue;
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[j]==0)continue;
-                int target = nums[i]+nums[j];
-                int index = bin_search(j+1,n-1,target,nums);
+        int n = nums.size(), ans = 0;
+        sort(nums.begin(), nums.end());
 
-                ans += (index-(j+1));
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    ans += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
             }
         }
+
         return ans;
     }
 };
