86b1b724e199bfa3c0961d0d1389ef67e25d0101
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-10-11 08:32:15 +0530

    Time: 291 ms (66.03%) | Memory: 191.6 MB (76.95%) - LeetSync

---
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long solve(int i, vector<int>& power, long long dp[], unordered_map<int,int>& mp) {
+        if (i >= power.size()) return 0;
+        if (dp[i] != -1) return dp[i];
+
+        // Option 1: skip
+        long long skip = solve(i + 1, power, dp, mp);
+
+        // Option 2: take and skip all numbers within diff <= 2
+        int next_index = i + 1;
+        while (next_index < power.size() && power[next_index] - power[i] <= 2) {
+            next_index++;
+        }
+
+        long long take = 1LL * power[i] * mp[power[i]] + solve(next_index, power, dp, mp);
+
+        return dp[i] = max(take, skip);
+    }
+
+    long long maximumTotalDamage(vector<int>& power) {
+        unordered_map<int,int> mp;
+        for (int x : power) mp[x]++;
+
+        sort(power.begin(), power.end());
+        power.erase(unique(power.begin(), power.end()), power.end());
+
+        long long dp[100005];
+        memset(dp, -1, sizeof(dp)); // ✅ initialize all to -1
+
+        return solve(0, power, dp, mp);
+    }
+};
