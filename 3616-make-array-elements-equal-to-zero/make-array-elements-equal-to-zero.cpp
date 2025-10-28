9d19fb780394735e65c2daee8f379ffd0223a162
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-10-28 08:02:37 +0530

    Time: 8 ms (40.83%) | Memory: 22.8 MB (34.86%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int n= nums.size(),count=0;
-        vector<int>prefix(n,0),suffix(n,0);
-        if(n==1 && nums[0]==0)return 2;
-        if(n==1 && nums[0]>1)return 0;
-        prefix[0]=nums[0],suffix[n-1]=nums[n-1];
+        vector<int>prefix(n,0);
+       
+        prefix[0]=nums[0];
+        int total_sum=nums[0];
         for(int i=1;i<n;i++)
         {
            prefix[i]=prefix[i-1]+nums[i];
+           total_sum+=nums[i];
         }
-        for(int i=n-2;i>=0;i--)
-        {
-           suffix[i]=suffix[i+1]+nums[i];
-        }
-
+         if(total_sum==0)return n*2;
+       
         for(int i=1;i<n-1;i++)
         {
-            if(nums[i]==0 && prefix[i-1]==suffix[i+1])
+            if(nums[i]==0 && prefix[i-1]==total_sum-prefix[i-1])
             {
                   count+=2;
             }
-            else if(nums[i]==0 && (prefix[i-1]+1 == suffix[i+1] || prefix[i-1]==suffix[i+1]+1))
+            else if(nums[i]==0 && (prefix[i-1]+1 == total_sum-prefix[i-1] || prefix[i-1]==total_sum-prefix[i-1]+1))
             {
                 count+=1;
             }
         }
-
-        if(nums[0]==0 && (prefix[n-1]==0) )count+=2;
-        
-        if(nums[0]==0 && (prefix[n-1]==1) )count+=1;
-
-         if(nums[n-1]==0 && (prefix[n-1]==0) )count+=2;
-         if(nums[n-1]==0 && (suffix[0]==1) )count+=1;
+       
+        if(nums[0]==0 && total_sum==1 )count+=1;
+        if(nums[n-1]==0 && total_sum==1)count+=1;
         
         return count;
 
