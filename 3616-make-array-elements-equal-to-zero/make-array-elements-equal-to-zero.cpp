7d9f4f743d883aca2424bfe25b1b2ccb959a000a
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-10-28 08:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (56.42%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int n= nums.size(),count=0;
-        vector<int>prefix(n,0);
+        
        
-        prefix[0]=nums[0];
+        int prefix=0;
         int total_sum=nums[0];
         for(int i=1;i<n;i++)
         {
-           prefix[i]=prefix[i-1]+nums[i];
            total_sum+=nums[i];
         }
          if(total_sum==0)return n*2;
        
-        for(int i=1;i<n-1;i++)
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]==0 && prefix[i-1]==total_sum-prefix[i-1])
+            prefix+=nums[i];
+            if(nums[i]==0 && prefix==total_sum-prefix)
             {
                   count+=2;
             }
-            else if(nums[i]==0 && (prefix[i-1]+1 == total_sum-prefix[i-1] || prefix[i-1]==total_sum-prefix[i-1]+1))
+            else if(nums[i]==0 && (prefix+1 == total_sum-prefix|| prefix==total_sum-prefix+1))
             {
                 count+=1;
             }
         }
        
-        if(nums[0]==0 && total_sum==1 )count+=1;
-        if(nums[n-1]==0 && total_sum==1)count+=1;
-        
         return count;
 
 
