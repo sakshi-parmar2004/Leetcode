cc3d34e04c143b5475ada3a90d1944b6d1b40c16
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-10-31 08:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (95.85%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-         vector<int>ans;
-         unordered_set<int>st;
-        for(int i:nums)
-        {
-            if(st.find(i)!=st.end())
-            {
-                ans.push_back(i);
-            }
-            st.insert(i);
-        }
-        return ans;
+        int xor_total = 0;
+        int n = nums.size();
         
+        // XOR all elements in nums
+        for (int x : nums) xor_total ^= x;
+
+        // XOR all numbers from 0 to n-3 (since two are duplicates)
+        for (int i = 0; i < n - 2; i++) xor_total ^= i;
+
+        // Now xor_total = a ^ b (the two sneaky numbers)
+
+        // Find rightmost set bit (position k)
+        int k = 0;
+        int temp = xor_total;
+        while ((temp & 1) == 0) {
+            k++;
+            temp >>= 1;
+        }
+
+        int first = 0, second = 0;
+
+        // Partition nums based on kth bit
+        for (int x : nums) {
+            if ((x >> k) & 1)
+                first ^= x;
+            else
+                second ^= x;
+        }
+
+        // Partition 0..n-3 based on kth bit
+        for (int i = 0; i < n - 2; i++) {
+            if ((i >> k) & 1)
+                first ^= i;
+            else
+                second ^= i;
+        }
+
+        return {first, second};
     }
 };
