b9377935361e56d29f91c74047f91f7a4e3d4e13
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-11-09 07:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (6.11%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int solve(int num1,int num2)
-    {
-        if(num1==0 || num2==0)
-        {
-            return 0;
-        }
-
-        if(num1>num2)
-        {
-            num1=num1-num2;
-        }
-        else
-        {
-           num2=num2-num1;
-        }
-        return 1+solve(num1,num2);
-    }
     int countOperations(int num1, int num2) {
-        
-        return solve(num1,num2);
+        int ops=0;
+        while(num1!=0 && num2!=0)
+        {
+            if(num2>num1)
+            {
+                ops+= (num2/num1);
+                num2=num2%num1;
+            }
+            else
+            {
+                  ops+= (num1/num2);
+                num1=num1%num2;
+
+            }
+        }
+        return ops;
     }
 };
