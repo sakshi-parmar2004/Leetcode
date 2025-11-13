beb368a837ddc9b3d24fd5c3066e02f65d343cb9
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-11-13 08:11:23 +0530

    Time: 12 ms (8.33%) | Memory: 21.2 MB (5.07%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int ans=0,ones=0;
-        int n= s.length();
-        for(int i=0;i<s.length()-1;i++)
-        {
-            if(s[i]=='0' && s[i+1]=='1'){ans+=ones;}
+        int n = s.length();
+        vector<int> arr(n, 0);
+        arr[0] = s[0] - '0';
 
-            if(s[i]=='1')ones++;
-        }
-        if(s[n-1]=='0')ans += ones;
-        return ans;
+        // build prefix sum of 1s
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] + (s[i] - '0');
         
+        }
+
+        int i = 0, j = 0, ans = 0;
+
+        while (j < n) {
+            // find end of current group of identical bits
+            while (j < n && arr[j] == arr[i]) {
+                j++;
+            }
+
+            // group is s[i..j-1], length = j-i
+            if (j - i > 1 && arr[i] != 0) {   // use arr[j-1] (end of group)
+                ans += arr[i];
+            }
+
+            i = j; // move i to start of next group
+        }
+
+        return ans;
     }
 };
