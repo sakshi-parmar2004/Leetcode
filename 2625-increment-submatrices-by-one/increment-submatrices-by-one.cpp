82b1a4b3834a9f2b371316eb5f7b4257f2103f8a
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-11-14 07:47:04 +0530

    Time: 12 ms (88.40%) | Memory: 86.4 MB (75.24%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        int m=queries.size();
+
         vector<vector<int>>ans(n,vector<int>(n,0));
-        for(int i=0;i<m;i++)
+        for(int i=0;i<queries.size();i++)
         {
-            int row_start=queries[i][0],col_start=queries[i][1]
+
+           int row_start=queries[i][0],col_start=queries[i][1]
             ,row_end=queries[i][2],col_end=queries[i][3];
 
-            for(int p=row_start;p<=row_end;p++)
-            {
-                for(int q=col_start;q<=col_end;q++)
-            {
-                ans[p][q]++;
+            ans[row_start][col_start]++;
+            if(row_end+1<n){
+            ans[row_end+1][col_start]--;
             }
+             if(col_end+1<n){
+            ans[row_start][col_end+1]--;
+             }
+              if(row_end+1<n && col_end+1<n ){
+            ans[row_end+1][col_end+1]++;
+              }
+
+
+
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                ans[i][j] += ans[i][j-1];
             }
-        
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                ans[i][j] += ans[i-1][j];
+            }
         }
         return ans;
-
-        
     }
 };
