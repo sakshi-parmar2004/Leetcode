3a18ede5b8d9d47d92c6b39454886f5826d6351e
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-11-17 12:12:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (40.94%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+         int total=n*(n+1)/2,sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=i;
+            if(sum*2==total+i)return i; 
+        }
+        return -1;
+    }
+};
