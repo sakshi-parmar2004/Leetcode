d144ad95f534a2c5d934b0b30bbb4601b87766a3
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-11-21 08:22:28 +0530

    Time: 163 ms (49.21%) | Memory: 16.1 MB (43.15%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         int ans=0;
-        unordered_map<int,vector<int>>mp;
+        unordered_set<char>st1;
         for(int i=0;i<s.length();i++)
         {
-            mp[s[i]].push_back(i);
-        }
-
-        for(auto i:mp)
-        {
-            vector<int>arr= i.second;
-            if(arr.size()==1)continue;
-            unordered_set<char>st;
-            if(arr.size()>2)st.insert(i.first);
-            for(int i=0;i<arr.size()-1;i++)
+            if(st1.find(s[i])!=st1.end())
             {
-                int l= arr[i],r=arr[i+1];
-                for(int j=l+1;j<r;j++)
-                {
-                    st.insert(s[j]);
-                }
+                continue;
+            }
+            st1.insert(s[i]);
+            int r = s.rfind(s[i]);
+            unordered_set<char>st;
+            for(int j=i+1;j<r;j++)
+            {
+                st.insert(s[j]);
             }
 
             ans+=st.size();
         }
-
         return ans;
+        
+    
     }
 
 };
