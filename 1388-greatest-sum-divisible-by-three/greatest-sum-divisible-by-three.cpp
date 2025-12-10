1f099fc583903cfe83f408b5d2cd4e67ed1deb62
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-12-10 18:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (58.95%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,28 +1,60 @@
 class Solution {
 public:
-    
-    int solve(int i, vector<int>& nums, int rem,vector<vector<int>>&dp) {
-      
-        if (i >= nums.size()){
-            if(rem==0)
-            {
-                return 0;
-            }
-            return INT_MIN;
-       }
-       if(dp[i][rem]!=-1)
-       {
-          return dp[i][rem];
-       }
-        int op1=solve(i + 1, nums, rem,dp);
-        int op2=nums[i]+solve(i + 1, nums, (rem + nums[i])%3,dp);
-        return dp[i][rem]= max(op1,op2);
-      
-    }
     int maxSumDivThree(vector<int>& nums) {
-        int rem = 0;
-        vector<vector<int>>dp(nums.size(),vector<int>(3,-1));
-        return solve(0, nums, rem,dp);
+        vector<int>one,two;
+        int sum=0;
+        for(int i:nums)
+        {
+            if(i%3==1)
+            {
+               one.push_back(i);
+            }
+            else if(i%3==2)
+            {
+                two.push_back(i);
+            }
+            sum+=i;
+        }
+        int rem =sum%3;
+        sort(one.begin(),one.end());
+        sort(two.begin(),two.end());
+        int ans=0;
+        if(rem==0)return sum ;
+        if(rem==1 )
+        {
+           int sm1=sum,sm2=sum;
+           if(!one.empty())
+           {
+             sm1-=one[0];
+             ans = max(ans,sm1);
+           }
+            
+            if(two.size()>=2)
+            {
+                sm2-= two[0];
+                sm2-=two[1];
+                ans=max(ans,sm2);
+            }
+
+            return ans;
+
+        }
+        else if(rem==2)
+        {
+            int sm1=sum,sm2=sum;
+            if(one.size()>=2)
+            {
+                sm1-=one[0];
+                sm1-=one[1];
+                ans=max(sm1,ans);
+            }
+            if(!two.empty())
+            {
+                ans= max(ans, sm2-two[0]);
+            }
+            return ans;
+        }
+        return 0;
         
     }
 };
