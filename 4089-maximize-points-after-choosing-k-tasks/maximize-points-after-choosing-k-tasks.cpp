d71a43bb9c5700989bdf03150399a549f1ce1502
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-12-13 16:16:34 +0530

    Time: 47 ms (74.39%) | Memory: 215.4 MB (62.98%) - LeetSync

--- 4089-maximize-points-after-choosing-k-tasks/maximize-points-after-choosing-k-tasks.cpp
+++ 4089-maximize-points-after-choosing-k-tasks/maximize-points-after-choosing-k-tasks.cpp
@@ -3,22 +3,27 @@ public:
     long long maxPoints(vector<int>& nums1, vector<int>& nums2, int k) {
         
         long long sum=0;
-        for(int i:nums1)
+        for(int i:nums2)
         {
             sum+=i;
         }
         vector<int>arr;
-        for(int i=0;i<nums2.size();i++)
+        for(int i=0;i<nums1.size();i++)
         {
-            int ele=(nums2[i]-nums1[i]);
+            int ele=(nums1[i]-nums2[i]);
             arr.push_back(ele);
         }
-sort(arr.begin(), arr.end(), greater<int>());
+       sort(arr.begin(), arr.end(), greater<int>());
 
-        int len= nums1.size()-k;
-        if(len==0)return sum;
+     
+        int n=nums2.size();
+        for(int i=0;i<k;i++)
+        {
+            sum+=arr[i];
+        }
+        
         long long maxi=0,sum2=0;
-        for(int i=0;i<len;i++)
+        for(int i=k;i<n;i++)
         {
             sum2+=arr[i];
             if(sum2<0)
